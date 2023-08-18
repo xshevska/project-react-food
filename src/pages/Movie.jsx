93aa302f4df22856f409f5c3b7422eb35c653bf2
Myroslava Shevska <miroslavashevskaya12@gmail.com>
2023-08-19 00:23:13 +0200

    Hools useLocation, useRouteMatch

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,8 +1,13 @@
-import {useParams, useHistory} from 'react-router-dom'
+import {useParams, useHistory, useRouteMatch, useLocation} from 'react-router-dom'
 
 function Movie() {
     const {title} = useParams();
     const {goBack} = useHistory()
+    const value = useRouteMatch();
+    const value2= useLocation();
+
+    console.log(value)
+    console.log(value2)
 
     return <>
         <h1>Some move {title}</h1>
